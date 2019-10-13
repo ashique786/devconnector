d976cb01383c001a43fa18dee6206a023b64d3d4
Ashique Rahman <pcashiquerahman@gmail.com>
2019-10-13 14:46:17 +0530

    Setting of routes

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports ={
+    mongoURI: "mongodb://ashiquerahman:surface313@ds333768.mlab.com:33768/connector"
+};
--- server.js
+++ server.js
@@ -1,9 +1,33 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 
+const users = require('./routes/api/users');
+const post = require('./routes/api/post');
+const profiles = require('./routes/api/profiles');
+
+//DB config
+
+const db = require('./config/keys').mongoURI;
+
+//connect to mongoDB through mongoose
+
+mongoose
+    .connect(db)
+    .then(() => {
+        console.log(' Hey Ashique MOngoDB connected')
+    })
+    .catch((err) => {
+        console.log(err)
+    });
 app.get('/', (req,res) =>{
     res.send('Hello rwetewg!')
 })
+
+app.use('/api/users/',users)
+app.use('/api/profiles/',profiles)
+app.use('/api/post/',post)
+
 const port = process.env.PORT || 5000;
 app.listen(port, () =>{
     console.log(`Server started on port ${port}`)
