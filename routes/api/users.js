66f94a1d3362551479ce2612011d7d1be284e8d1
Ashique Rahman <pcashiquerahman@gmail.com>
2019-10-17 23:06:45 +0530

    login functionality ie: check for email existance if so validate password

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,4 +47,27 @@ const User = require('../../models/Users');
         });
  })
 
+//@login
+ router.post('/login', (req,res) =>{
+     const email = req.body.email;
+     const password = req.body.password;
+     //find user by mail
+     User.findOne({email})
+        .then((user) => {
+            if(!user){
+                return res.status(404).json({email: "user not found"});
+            }
+            //Check password 
+            bcrypt.compare(password, user.password)
+              .then(isMatch =>{
+                  if(isMatch){
+                      res.json({msg:'Success'})
+                  }else{
+                    return res.status(400).json({password:'Password incorrect'})
+                  }
+              })
+        })
+ })
+
+
  module.exports = router;
